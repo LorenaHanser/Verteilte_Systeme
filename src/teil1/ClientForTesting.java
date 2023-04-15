b9df7fa7ef0e11ddb582cf83d4b6c7f1daec7e7c
ds704 <42025411+ds704@users.noreply.github.com>
2023-04-15 11:36:13 +0200

    add Client for Test and Debuging

--- src/teil1/ClientForTesting.java
+++ src/teil1/ClientForTesting.java
@@ -13,44 +13,6 @@ import java.net.UnknownHostException;
  * @author www.codejava.net
  */
 public class ClientForTesting {
-    private String hostname;
-    private int port;
-    private String userName;
-
-    // Konstruktor
-
-    public ClientForTesting(String hostname, int port) {
-        this.hostname = hostname;
-        this.port = port;
-    }
-
-    public void execute() {
-        try {
-            Socket socket = new Socket(hostname, port);
-
-            System.out.println("Connected to the chat server");
-
-            new ClientReadThread(socket, this).start();
-            new ClientWriteThread(socket, this).start(); //hier starten wir die Threads
-
-        } catch (UnknownHostException ex) {
-            System.out.println("Die Konfiguration stimmt nicht: " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("Der Server " + port + " ist nicht online: " + ex.getMessage() + ". Probiere erneut");
-            main(new String[1]);
-        }
-
-    }
-
-    void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    String getUserName() {
-        return this.userName;
-    }
-
-
     public static void main(String[] args) {
         try {
             BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
@@ -67,7 +29,7 @@ public class ClientForTesting {
                     System.out.println("Leider ist die Eingabe nicht korrekt");
                 }
             }
-            ClientForTesting client = new ClientForTesting(hostname, port);
+            Client client = new Client(hostname, port);
             client.execute();
         } catch (IOException e) {
             throw new RuntimeException(e);
