a44e5421ab2cc828fcf4843e8065b5869a94285a
DDanny1999 <90893899+DDanny1999@users.noreply.github.com>
2023-04-22 23:37:53 +0200

    some comments

--- src/teil1/Client.java
+++ src/teil1/Client.java
@@ -3,9 +3,16 @@ package teil1;
 import java.net.*;
 import java.io.*;
 
+/**
+ * Der Client:
+ *
+ * Hier wird der Client gestartet und verbindet sich über Lastverteilung (math.random)
+ * mit einem zufälligen Server.
+ * Außerdem werden die Threads ClientReadThread & ClientWriteThread gestartet
+ */
 public class Client {
 
-    private String hostname;
+    private final String hostname;
     private int port;
     private String userName;
 
@@ -17,36 +24,30 @@ public class Client {
 
     public void execute() {
         try {
-            Socket socket = new Socket(hostname, port);
+            Socket socket = new Socket(hostname, port);           // Verbindung mit einem Server aufbauen
 
             System.out.println(Server.ANSI_PURPLE + "Connected to the chat server (port: " + port + ")" + Server.ANSI_RESET);
 
-            new ClientReadThread(socket, this).start();
-            new ClientWriteThread(socket, this).start(); //hier starten wir die Threads
+            new ClientReadThread(socket, this).start();     // hier starten wir den ClientReadThread
+            new ClientWriteThread(socket, this).start();    // hier starten wir den ClientWriteThread
 
         } catch (UnknownHostException ex) {
             System.out.println(Server.ANSI_RED + "Die Konfiguration stimmt nicht: " + ex.getMessage() + Server.ANSI_RESET);
         } catch (IOException ex) {
-            System.out.println(Server.ANSI_RED + "Der Server " + port + " ist nicht online: " + ex.getMessage() + ".\nProbiere erneut." + Server.ANSI_RESET);
-            main(new String[1]);
+            System.out.println(Server.ANSI_RED + "Der Server " + port + " ist nicht online: " + ex.getMessage() + ".\nProbiere erneut." + Server.ANSI_RESET);   // Falls Server mit gewähltem Port nicht online,
+            main(new String[1]);                                                                                                                                // versuche erneut
         }
-
     }
 
     void setUserName(String userName) {
         this.userName = userName;
     }
 
-    String getUserName() {
-        return this.userName;
-    }
-
     public static void main(String[] args) {
-
-        String hostname = "localhost";//args[0];
+        String hostname = "localhost";
         int port = 8989;                                    // Server 1 hat immer Port 8989 Server 2 8990
         int randomNumber = (int) (Math.random() * 2);       // Lastverteilung mit Math.random()
-        port = port + randomNumber;
+        port = port + randomNumber;                         // 8989 + 0 oder 1
 
         Client client = new Client(hostname, port);
         client.execute();
